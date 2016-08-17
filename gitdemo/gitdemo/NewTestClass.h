275aeda2a6d29ed7c0de3c11baf3a383aab97cfd
wukexiu <biandewen@biandewendeMacBook-Pro.local>
2016-08-17 16:23:27 +0800

    add NewTestClass

---
+++ gitdemo/gitdemo/NewTestClass.h
@@ -0,0 +1,13 @@
+//
+//  NewTestClass.h
+//  gitdemo
+//
+//  Created by wukexiu on 16/8/17.
+//  Copyright © 2016年 biandewen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewTestClass : NSObject
+
+@end
---
+++ gitdemo/gitdemo/NewTestClass.m
@@ -0,0 +1,13 @@
+//
+//  NewTestClass.m
+//  gitdemo
+//
+//  Created by wukexiu on 16/8/17.
+//  Copyright © 2016年 biandewen. All rights reserved.
+//
+
+#import "NewTestClass.h"
+
+@implementation NewTestClass
+
+@end
