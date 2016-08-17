563e38ac41bd669b22a0588f98a0e7802d28ed48
wukexiu <biandewen@biandewendemacbook-pro.local>
2016-08-17 15:37:53 +0800

    add ModeDataInfo

---
+++ gitdemo/gitdemo/ModeDataInfo.h
@@ -0,0 +1,13 @@
+//
+//  ModeDataInfo.h
+//  gitdemo
+//
+//  Created by wukexiu on 16/8/17.
+//  Copyright © 2016年 biandewen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ModeDataInfo : NSObject
+
+@end
---
+++ gitdemo/gitdemo/ModeDataInfo.m
@@ -0,0 +1,13 @@
+//
+//  ModeDataInfo.m
+//  gitdemo
+//
+//  Created by wukexiu on 16/8/17.
+//  Copyright © 2016年 biandewen. All rights reserved.
+//
+
+#import "ModeDataInfo.h"
+
+@implementation ModeDataInfo
+
+@end
